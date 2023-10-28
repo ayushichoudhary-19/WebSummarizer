6f8d5cf67f0ca029550e0bbd151ddd0ee873940d
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2023-10-28 09:43:27 +0530

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOMContentLoaded event fired");
-    // Extract the page content
-    const pageContent = document.body.innerText;
-    console.log(pageContent);
-    // Send the page content to the background script
-    chrome.runtime.sendMessage({ action: "pageContent", content: pageContent });
-});
